0caaffe4d9f3eaca1c7bee8d3f9fbd432284cad4
srikumaran <srik24946@gmail.com>
2025-07-20 19:59:19 +0530

    Extra Reducers & Async Thunk Set

--- src/store/postslice.js
+++ src/store/postslice.js
@@ -1,11 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+
+export const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+   async ({ query }, thunkAPI) => { 
+
+    }
+);
+const initialState = {
+    videos: [],
+    loading: false,
+}
 
 export const postSlicer = createSlice({
     name:"post",
-    initialState:{},
+    initialState,
     reducers:{
 
-    }
+    },
+     extraReducers: (builder) => {
+        builder
+            .addCase(fetchVideos.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchVideos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.videos = action.payload;
+            })
+            .addCase(fetchVideos.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
 export const {} = postSlicer.actions;
